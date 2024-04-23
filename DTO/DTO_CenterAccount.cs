c107bbfc57aad60cd929ebd99d3ae46bd46db83b
Najin-Hades <tnthien165.truong@gmail.com>
2024-04-23 20:00:20 +0700

    Center_Account changed

--- DTO/DTO_CenterAccount.cs
+++ DTO/DTO_CenterAccount.cs
@@ -7,13 +7,19 @@ using System.Threading.Tasks;
 namespace DTO {
     public class DTO_CenterAccount : DTO_Account{
         //Properties
-        public List<string > NumberPhones = new List<string>();
-        public List<string> UsersName = new List<string>();
+        public string numberPhone;
+        public List<string> staffs = new List<string>();
 
         //Constructor
         public DTO_CenterAccount(string username, string password, DateTime dateCreated, DateTime lastloginDate): 
             base(username,password,dateCreated,lastloginDate,"Staff") {
         }
+        public void themNhanVien(string staff) {
+            staffs.Add(staff);
+        }
+
+        
+
         
     }
 }
